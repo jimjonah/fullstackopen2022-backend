cd70fd316fd4d6158bb5712a31faef719deba8a7
jjonah <jjonah@veracode.com>
2023-03-17 09:17:05 -0400

    3.18*: Phonebook database step6

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -24,33 +24,35 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ');
 }));
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-];
+// let persons = [
+//   {
+//     "id": 1,
+//     "name": "Arto Hellas",
+//     "number": "040-123456"
+//   },
+//   {
+//     "id": 2,
+//     "name": "Ada Lovelace",
+//     "number": "39-44-5323523"
+//   },
+//   {
+//     "id": 3,
+//     "name": "Dan Abramov",
+//     "number": "12-43-234345"
+//   },
+//   {
+//     "id": 4,
+//     "name": "Mary Poppendieck",
+//     "number": "39-23-6423122"
+//   }
+// ];
 
 app.get('/info', (request, response) => {
-  let infoStr = `Phonebook has info for ${persons.length} people`;
-  response.send(`<p>${infoStr}</p>
-                <p>${new Date()}</p>`
+  Person.countDocuments({}).then(count => {
+        let infoStr = `Phonebook has info for ${count} people`;
+        response.send(`<p>${infoStr}</p>
+                    <p>${new Date()}</p>`);
+      }
   );
 });
 
@@ -64,10 +66,11 @@ app.get('/api/persons', (request, response) => {
   });
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById({_id: request.params.id}).then(person => {
     response.json(person);
-  });
+  })
+  .catch(error => next(error))
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
