4a1a688a3ee630487c1250f0b9f17cbb675aa9e7
jjonah <jjonah@veracode.com>
2023-04-12 15:57:37 -0600

    4.4: helper functions and unit tests, step2

--- bloglist/tests/blogs.test.js
+++ bloglist/tests/blogs.test.js
@@ -1,8 +1,36 @@
-const listHelper = require('../utils/list_helper')
+const listHelper = require('../utils/list_helper');
+const Blog = require('../models/blog');
 
-test('dummy returns one', () => {
-  const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
-})
+describe('blogs test', () => {
+  test('dummy returns one', () => {
+    const blogs = [];
+
+    const result = listHelper.dummy(blogs);
+    expect(result).toBe(1);
+  });
+
+  test('return total of all likes', () => {
+    let blogs = [];
+
+    let result = listHelper.totalLikes(blogs);
+    expect(result).toBe(0);
+
+    blogs.push({
+      "title": "New Blog Title",
+      "author": "Jim Jonah",
+      "url": "www.mysite.com",
+      "likes": 3
+    });
+
+    blogs.push({
+      "title": "Second Blog Title",
+      "author": "Jim Jonah",
+      "url": "www.mysite.com",
+      "likes": 2
+    });
+
+    result = listHelper.totalLikes(blogs);
+    expect(result).toBe(5);
+  });
+});
