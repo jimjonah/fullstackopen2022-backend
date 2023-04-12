7dae38c9e1b0217bdd385076914a4e9d55ac9b30
jjonah <jjonah@veracode.com>
2023-04-12 13:20:37 -0600

    4.2 Blog list, step2

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,43 +1,34 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+
+// ==========
+mongoose.set('strictQuery', false)
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+.then(() => {
+  logger.info('connected to MongoDB')
 })
+.catch((error) => {
+  logger.error('error connecting to MongoDB:', error.message)
+})
+//=============
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+// const mongoUrl = 'mongodb://localhost/bloglist'
+// mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-  .find({})
-  .then(blogs => {
-    response.json(blogs)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-  .save()
-  .then(result => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
 
