d59eb2968465bbbadc89d9547a4e334994d610df
jjonah <jjonah@veracode.com>
2023-04-12 15:58:36 -0600

    4.4: helper functions and unit tests, step2

--- bloglist/tests/blogs.test.js
+++ bloglist/tests/blogs.test.js
@@ -1,5 +1,4 @@
 const listHelper = require('../utils/list_helper');
-const Blog = require('../models/blog');
 
 
 describe('blogs test', () => {
@@ -34,3 +33,22 @@ describe('blogs test', () => {
     expect(result).toBe(5);
   });
 });
+
+//another way to test the blogs
+// describe('total likes', () => {
+//   const listWithOneBlog = [
+//     {
+//       _id: '5a422aa71b54a676234d17f8',
+//       title: 'Go To Statement Considered Harmful',
+//       author: 'Edsger W. Dijkstra',
+//       url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+//       likes: 5,
+//       __v: 0
+//     }
+//   ]
+//
+//   test('when list has only one blog, equals the likes of that', () => {
+//     const result = listHelper.totalLikes(listWithOneBlog)
+//     expect(result).toBe(5)
+//   })
+// })
